10fe0aad41ab5e9bd8ce9b05dee6c0353d0c54eb
clarkio <brian@clarkio.com>
2017-12-27 09:48:09 -0500

    init

---
+++ wifiname/wifiname/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  wifiname
+//
+//  Created by Brian Clark on 12/27/17.
+//  Copyright © 2017 Clarkio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ wifiname/wifiname/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wifiname
+//
+//  Created by Brian Clark on 12/27/17.
+//  Copyright © 2017 Clarkio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
